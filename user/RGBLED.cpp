4f6602a8242d5b7ad6d165a261ec7fca6bd91dcf
Daniel Driezshuk <d.drizhuk@gmail.com>
2016-03-03 13:50:21 +0300

    Fix for standard sntp.h

--- user/RGBLED.cpp
+++ user/RGBLED.cpp
@@ -13,6 +13,21 @@ extern "C"{
 #include <pwm.h>
 #include <ctype.h>
 #include <sntp.h>
+
+	typedef long time_t;
+	struct tm
+	{
+	  int	tm_sec;
+	  int	tm_min;
+	  int	tm_hour;
+	  int	tm_mday;
+	  int	tm_mon;
+	  int	tm_year;
+	  int	tm_wday;
+	  int	tm_yday;
+	  int	tm_isdst;
+	};
+	struct tm * sntp_localtime(const time_t * tim_p);
 }
 #include "cJSON.h"
 #include "IoTServer.h"
